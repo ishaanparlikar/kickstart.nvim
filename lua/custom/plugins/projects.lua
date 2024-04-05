471285fa857510305a52b43d76e9d4475edb1f71
ishaanparlikar <iparlika@emich.edu>
2024-04-05 13:35:48 +0530

    added projects plugin for telescope

--- lua/custom/plugins/projects.lua
+++ lua/custom/plugins/projects.lua
@@ -1,10 +1,11 @@
 return {
-    "ahmedkhalf/project.nvim",
+  'ahmedkhalf/project.nvim',
   config = function()
-    require("project_nvim").setup {
+    require('project_nvim').setup {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json' },
     }
-end
+  end,
 }

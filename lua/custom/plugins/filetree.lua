a1ced93901ee0df6fe2b03a11350cb8da1b956f5
Ishan Parlikar <iparlika@emich.edu>
2024-04-18 21:03:57 +0530

    filetree update

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,15 +1,19 @@
 -- File: lua/custom/plugins/filetree.lua
 
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = { 
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    },
-    config = function ()
-      require('nvim-tree').setup {
-      }
-    end,
-  }
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('nvim-tree').setup {
+      view = {
+        side = 'right',
+        width = 40,
+      },
+    }
+  end,
+}

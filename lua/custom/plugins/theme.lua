d09917e17e8e5bff7ea0c7bd2a1e8b5470aae5a8
Ishan Parlikar <iparlika@emich.edu>
2024-04-18 21:04:04 +0530

    added themes

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,6 +1,26 @@
-return {
-  "eldritch-theme/eldritch.nvim",
+local poimandres = {
+  'olivercederborg/poimandres.nvim',
   lazy = false,
   priority = 1000,
-  opts = {},
+  config = function()
+    require('poimandres').setup {
+      disable_italics = false,
+    }
+  end,
+}
+
+local onedark = {
+  'navarasu/onedark.nvim',
+  lazy = false,
+  config = function()
+    require('onedark').setup {}
+  end,
+}
+
+local eldritch = { 'eldritch-theme/eldritch.nvim', lazy = false, priority = 1000, opts = {} }
+
+return {
+  poimandres,
+  eldritch,
+  onedark,
 }

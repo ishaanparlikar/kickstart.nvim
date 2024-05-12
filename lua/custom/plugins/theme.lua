f7bcaa0d80332bb63f5de351acbead4b63e58327
Ishan Parlikar <iparlika@emich.edu>
2024-05-12 18:00:17 +0530

    removed unused themes

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,69 +1,20 @@
-local poimandres = {
-  'olivercederborg/poimandres.nvim',
-  lazy = false,
-  -- priority = 1000,
-  config = function()
-    require('poimandres').setup {
-      disable_italics = false,
-    }
-  end,
-}
-
-local onedark = {
-  'navarasu/onedark.nvim',
-  lazy = false,
-  config = function()
-    require('onedark').setup {
-      style = 'deep',
-    }
-  end,
-}
-
-local github_dark = {
-  'projekt0n/github-nvim-theme',
-  lazy = false,
-  config = function()
-    require('github-theme').setup {}
-  end,
-}
-
-local night_owl = {
-  'oxfist/night-owl.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  -- priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- load the colorscheme here
-    require('night-owl').setup()
-    vim.cmd.colorscheme 'night-owl'
-  end,
-}
-
 local catppuccin = {
   'catppuccin/nvim',
-}
-
-local eldritch = { 'eldritch-theme/eldritch.nvim', lazy = false, priority = 1000, opts = {} }
-
-local material = {
-  'marko-cerovac/material.nvim',
-  lazy = false,
   config = function()
-    require('material').setup {}
+    require('catppuccin').setup {}
   end,
 }
 
-local onedark_pro = {
-  'olimorris/onedarkpro.nvim',
-  -- priority = 1000, -- Ensure it loads first
+local kanagawa = {
+  'rebelot/kanagawa.nvim',
+  priority = 1000, -- make sure to load this before all the other start plugins
+  lazy = false,
+  config = function()
+    require('kanagawa').setup {}
+  end,
 }
 
 return {
-  poimandres,
-  eldritch,
-  onedark_pro,
-  onedark,
-  github_dark,
-  night_owl,
   catppuccin,
-  material,
+  kanagawa,
 }

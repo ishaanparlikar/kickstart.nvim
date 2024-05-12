2af47f7dcc7c5982d75304225a8a85b261917cfa
Ishan Parlikar <iparlika@emich.edu>
2024-05-12 17:59:45 +0530

    added default language servers

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -177,6 +177,12 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'eslint_d',
+        'lua-language-server',
+        'prettierd',
+        'svelte-language-server',
+        'tailwindcss-language-server',
+        'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 

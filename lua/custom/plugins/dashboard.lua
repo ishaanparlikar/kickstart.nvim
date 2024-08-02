bff45d098f33afcecdfa9fceb3c1729e5ff20b2e
ishaanparlikar <iparlika@emich.edu>
2024-08-02 16:38:13 +0530

    added new plugins

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -1,42 +1,47 @@
 return {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        theme = 'doom',
-        config = {
-            header = {}, --your header
-            center = {
-              {
-                icon = ' ',
-                icon_hl = 'Title',
-                desc = 'Find File           ',
-                desc_hl = 'String',
-                key = 'b',
-                keymap = 'SPC f f',
-                key_hl = 'Number',
-                key_format = ' %s', -- remove default surrounding `[]`
-                action = 'lua print(2)'
-              },
-              {
-                icon = ' ',
-                desc = 'Find Dotfiles',
-                key = 'f',
-                keymap = 'SPC f d',
-                key_format = ' %s', -- remove default surrounding `[]`
-                action = 'lua print(3)'
-              },
-              {
-                icon = ' ',
-                desc = 'Find Projects',
-                key = 'p',
-                keymap = 'SPC',
-                key_format = ' %s', -- remove default surrounding `[]`
-                action = 'lua print(3)'
-              },
-            },
-            footer = {}  --your footer
-          }
-      }
-    end,
-  }
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      theme = 'doom',
+      config = {
+        header = {}, --your header
+        center = {
+          {
+            icon = ' ',
+            icon_hl = 'Title',
+            desc = 'Find File           ',
+            desc_hl = 'String',
+            key = 'b',
+            keymap = 'SPC f f',
+            key_hl = 'Number',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = 'lua print(2)',
+          },
+          {
+            icon = ' ',
+            desc = 'Find Dotfiles',
+            key = 'f',
+            keymap = 'SPC f d',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = 'lua print(3)',
+          },
+          {
+            icon = ' ',
+            desc = 'Find Projects',
+            key = 'p',
+            keymap = 'SPC',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = 'lua print(3)',
+          },
+        },
+        footer = function()
+          local stats = require('lazy').stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+        end,
+      },
+    }
+  end,
+}
+

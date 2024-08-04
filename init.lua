9d6dd7910468c179c2ea73b8e229171850ac5eec
ishaanparlikar <iparlika@emich.edu>
2024-08-04 14:51:54 +0530

    added bufferline

--- init.lua
+++ init.lua
@@ -19,3 +19,5 @@ require 'lazy-plugins'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 vim.cmd.colorscheme 'tokyonight-night'
+
+require('bufferline').setup()

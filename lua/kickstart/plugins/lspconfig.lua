107957ebc0f63a3a25fef83d8eb2ac572f5570de
ishaanparlikar <iparlika@emich.edu>
2025-02-13 09:39:16 +0530

    added globals vim and snacks

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -190,7 +190,13 @@ return {
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                globals = {
+                  'vim',
+                  'Snacks',
+                },
+                disable = { 'missing-fields' },
+              },
             },
           },
         },
